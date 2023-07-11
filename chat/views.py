e1738dada22dd9eb318e32a8c2f1a9da31ebc01f
Goddy01 <adigungodwin2@gmail.com>
2023-07-11 18:41:06 +0100

    added the create_room method in chat.views

--- chat/views.py
+++ chat/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+import json
+from .models import Room
 # Create your views here.
+
+@require_POST
+def create_room(request):
+    name = request.POST.get('name', '')
+    url = request.POST.get('url', '')
+
+    Room.objects.create(
+        client=name,
+        url=url
+    )
+    return JsonResponse({'message': 'room created'})
