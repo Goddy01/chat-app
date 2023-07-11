0f01709a78f3f48df7d471d75101e70403d212ec
Goddy01 <adigungodwin2@gmail.com>
2023-07-11 12:18:07 +0100

    set up the model for 'Room'

--- chat/models.py
+++ chat/models.py
@@ -7,9 +7,35 @@ class Message(models.Model):
     body =          models.TextField()
     sent_by =       models.CharField(max_length=255)
     created_at =    models.DateTimeField(auto_now_add=True)
-    created_by =    models.ForeignKey(User, on_delete=models.SET_NULL, blank=True)
+    created_by =    models.ForeignKey(User, on_delete=models.SET_NULL, blank=True, null=True)
     class Meta:
-        ordering = ('created_at')
+        ordering = ('created_at', ) # latest at the bottom 
 
     def __str__(self):
         return f"{self.sent_by}"
+    
+class Room(models.Model):
+    WAITING = 'waiting'
+    ACTIVE = 'active'
+    CLOSED = 'closed'
+
+    CHOICES_STATUS = (
+        (WAITING, 'waiting'),
+        (ACTIVE, 'active'),
+        (CLOSED, 'closed'),
+    )
+
+
+    uuid =          models.CharField(max_length=255)
+    client =        models.CharField(max_length=255)
+    agent =         models.ForeignKey(User, null=True, blank=True, on_delete=models.SET_NULL)
+    created_at =    models.DateTimeField(auto_now_add=True)
+    messages =      models.ManyToManyField(Message, blank=True)
+    url =           models.CharField(max_length=255, blank=True, null=True)
+    status =        models.CharField(max_length=32, choices=CHOICES_STATUS, default=WAITING)
+
+    class Meta:
+        ordering = ('-created_at', ) # latest at the top
+
+    def __str__(self):
+        return f"{self.uuid} - {self.client}"
