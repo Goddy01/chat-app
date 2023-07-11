38db480f88d4a7cd266f952db01eb071b9527148
Goddy01 <adigungodwin2@gmail.com>
2023-07-11 14:12:31 +0100

    added chat_bubble components

--- static/js/main.js
+++ static/js/main.js
@@ -1,1 +1,15 @@
+const chatElement = document.querySelector('#chat')
+const chatOpenElement = document.querySelector('#chat_open')
+const chatJoinElement = document.querySelector('#chat_join')
+const chatIconElement = document.querySelector('#chat_icon')
+const chatWelcomeElement = document.querySelector('#chat_welcome')
 
+
+chatOpenElement.onclick = function (e) {
+    e.preventDefault()
+
+    chatIconElement.classList.add('hidden');
+    chatWelcomeElement.classList.remove('hidden');
+
+    return false;
+}
